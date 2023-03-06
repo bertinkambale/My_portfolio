88a19ecf6d1960c7592750b3289ea70ff1c0d75d
Bertinkambale <bertinkambale988@gmail.comgit config --global user.email bertinkambale988@gmail.com>
2023-03-06 23:12:58 +0200

    fix expected break

--- javascript.js
+++ javascript.js
@@ -1,17 +1,17 @@
 const hamberger = document.querySelector('.hamburger');
 const navlink = document.querySelector('.menu');
-const closeButton = document.querySelector('.close-btn')
+const closeButton = document.querySelector('.close-btn');
 
 hamberger.addEventListener('click', () => {
-     navlink.style.display = 'initial';
+  navlink.style.display = 'initial';
 });
 
 closeButton.addEventListener('click', () => {
-     navlink.style.display = 'none';
+  navlink.style.display = 'none';
 });
 
 navlink.addEventListener('click', (event) => {
-     if (event.target.tagName === 'A') {
-          navlink.style.display = 'none'; 
-     }
+  if (event.target.tagName === 'A') {
+    navlink.style.display = 'none';
+  }
 });
