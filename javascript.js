e076f7b174d3c64bfcc1c0e72dbbf7d56c87fa22
skabeo <spencerokyere95@gmail.com>
2023-03-06 15:52:01 +0000

    add functionality to the close button

--- javascript.js
+++ javascript.js
@@ -1,6 +1,11 @@
 const hamberger = document.querySelector('.hamburger');
 const navlink = document.querySelector('.menu');
+const closeButton = document.querySelector('.close-btn')
 
 hamberger.addEventListener('click', () => {
      navlink.style.display = 'initial';
+});
+
+closeButton.addEventListener('click', () => {
+     navlink.style.display = 'none';
 });
