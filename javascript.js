d1dc676b34c9b0b0a5e2a158ac0e46d622ddbae5
Bertinkambale <bertinkambale988@gmail.comgit config --global user.email bertinkambale988@gmail.com>
2023-03-06 17:07:33 +0200

    make the humberger button clickable

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const hamberger = document.querySelector('.hamburger');
+const navlink = document.querySelector('.menu');
+
+hamberger.addEventListener('click', () => {
+     navlink.style.display = 'initial';
+});
